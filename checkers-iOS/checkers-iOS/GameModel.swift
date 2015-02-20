4e11f289dab9d0f3f62f8325688f19b2006d7de4
rictyran <rictyran@Richards-MacBook-Air.local>
2015-02-20 14:03:30 -0500

    added part of game logic

---
+++ checkers-iOS/checkers-iOS/GameModel.swift
@@ -0,0 +1,79 @@
+//
+//  GameModel.swift
+//  checkers-iOS
+//
+//  Created by Richard Tyran on 2/19/15.
+//  Copyright (c) 2015 Bobby Towers. All rights reserved.
+//
+
+import UIKit
+
+private let _mainData = DataModel()
+
+class DataModel: NSObject {
+    
+    var allGames: [GameModel] = []
+    var currentGame: GameModel?
+    
+    class func mainData() -> DataModel { return _mainData }
+    
+}
+
+// board positions
+
+class GameModel: NSObject {
+    
+    /// 0 = empty, 1 = player1, 2 = player2
+    
+    let boardSquares = [
+        
+        [0,1,0,1,0,1,0,1],
+        [1,0,1,0,1,0,1,0],
+        [0,1,0,1,0,1,0,1],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [2,0,2,0,2,0,2,0],
+        [0,2,0,2,0,2,0,2],
+        [2,0,2,0,2,0,2,0]
+        
+    ]
+    
+    var boardPieces: [[GamePiece?]] = Array(count: 8, repeatedValue: Array(count: 8, repeatedValue: nil))
+    
+    // board squares [row][col]
+    
+    
+    //players
+    
+    // you can use this direction when testing moves available
+    
+    var players: [Player] = [Player(direction: 1),Player(direction: -1)]
+    
+    
+    
+    // winner
+    var winner: Player?
+    var isDraw = false
+    
+    // moves made
+    // kings per player
+    
+}
+
+// var oppositeRow = player.direction == 1 ? 7 : 0
+
+class Player: NSObject {
+    
+    var direction: Int!
+    
+    init(direction: Int) {
+        
+        super.init()
+        self.direction = direction
+        
+        
+        
+    }
+    
+}
+      
--- checkers-iOS/checkers-iOS/GamePiece.swift
+++ checkers-iOS/checkers-iOS/GamePiece.swift
@@ -6,6 +6,22 @@
 
 import UIKit
 
+struct boardLocation {
+    
+    var x: Int
+    var y: Int
+    
+    
+    
+}
+
+
+
+protocol GamePieceDelegate {
+    
+    func pieceSelected(piece: GamePiece)
+    
+}
 
 enum PieceType: Int {
     
@@ -20,6 +36,20 @@ enum PieceType: Int {
 
 class GamePiece: UIView {
 
+    var player: Player? {
+        
+        var index = 0
+        
+        let playerIndex = (type.rawValue - 1) % 2
+        return DataModel.mainData().currentGame?.players[playerIndex]
+        
+    }
+    
+    ///(col,row) (x,y)
+    var square: (Int,Int)!
+    
+    var delegate: GamePieceDelegate?
+
     var type: PieceType!
     
     init(type: PieceType) {
@@ -30,7 +60,7 @@ class GamePiece: UIView {
         backgroundColor = type.hashValue % 2 == 0 ? UIColor.whiteColor() : UIColor.grayColor()
         
         layer.cornerRadius = 10
-        
+
         
     }
 
@@ -38,7 +68,24 @@ class GamePiece: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        
+        // piece selected
+        // change color for selection
+        
+        delegate?.pieceSelected(self)
+        
+        if let touch = touches.allObjects.last as? UITouch {
+            
+            let location = touch.locationInView(self)
+            
+            let col = 
+            let row =
+            
+            let selectedPiece = DataModel.mainData().currentGame?.boardPieces[row][col]
+        }
+        
+    }
     
     /*
     // Only override drawRect: if you perform custom drawing.
